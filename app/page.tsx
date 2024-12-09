16496467a340fce3443165c3561cd6778ff71bb1
Ved Nig <vednig12@gmail.com>
2024-12-09 12:49:33 +0530

    Titlle Set

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import CountdownTimer from "@/components/countdown-timer";
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'DemoDay - Pitch Perfect',
+  description: 'DemoDay is a global event for startups to showcase their products',
+}
+ 
+
 export default function Home() {
+  
   return (
     <CountdownTimer/>
      );
